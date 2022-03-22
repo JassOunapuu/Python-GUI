253c7e50843f52a5286672d1cc6099ce42d60a3b
JassOunapuu <98941717+JassOunapuu@users.noreply.github.com>
2022-03-22 13:18:20 +0200

    Add files via upload

--- GUI 05.py
+++ GUI 05.py
@@ -2,6 +2,7 @@
 #  15.03.2022
 #    IT-21
 
+import math
 from tkinter import *
 import tkinter.ttk
 
@@ -11,7 +12,7 @@ aken.title('Nupud')
 aken.geometry("300x200")
 
 #sildid
-silt = Label(aken, text="Käibemaks:")
+silt = Label(aken, text="Vali käibemaks:")
 silt.grid(row=1, column=0)
 silt1 = Label(aken, text="Hind käibemaksuta:")
 silt1.grid(row=0, column=0)
@@ -38,19 +39,16 @@ def arvuta():
     vari = var.get()
     sis = sisestus.get()
     if vari == 1:
-        arv = float(sis)*0.09
+        arv = round(float(sis)*0.09,2)
     else:
-        arv = float(sis)*0.2
+        arv = round(float(sis)*0.2,2)
+        
+        
+        
         
     kmaks = float(arv) + float(sis)
-    silt2 =Label(aken, text=arv)
-    silt2.grid(row=4, column=1)
-    silt3 =Label(aken, text=kmaks)
-    silt3.grid(row=5, column=1)    
-    
-    print(var.get())
-    print(sisestus.get())
-
+    silt2.config(text=arv)
+    silt3.config(text=kmaks)
 
 
 #nupud
@@ -62,6 +60,7 @@ silt3 =Label(aken, text=0.00)
 silt3.grid(row=5, column=1)
 
 # joon
-tkinter.ttk.Separator(aken, orient=HORIZONTAL).grid(column=1, row=1, rowspan=10, sticky='we')
+silt = Label(aken, text="____________________________________________________________")
+silt.grid(row=3, columnspan=2)
 
 aken.mainloop()
