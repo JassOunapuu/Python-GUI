9e6a9cd9e82300b35730dcd5d63ecb27603d4a86
JassOunapuu <98941717+JassOunapuu@users.noreply.github.com>
2022-03-24 15:26:39 +0200

    Add files via upload

--- GUI 06.py
+++ GUI 06.py
@@ -2,19 +2,17 @@
 #  22.03.2022
 #    IT-21
 
-import tkinter as tk
+
 from tkinter import *
-import time
-from PIL import ImageTk, Image
 
 #akna seaded
 aken = Tk()
 aken.title('Joonistamine')
-aken.geometry("500x500")
+aken.geometry("800x400")
 
 
 #lõuendi loomine
-louend = Canvas(aken, width=500, height=500)
+louend = Canvas(aken, width=800, height=500)
 louend.pack()
 
 
@@ -32,13 +30,11 @@ louend.create_rectangle(0, 180, 350, 280, fill="#02b2f2", outline="#ffffff")
 silt = Label(aken, text="Botswana lipp")
 silt.place(x=150, y=280)
 
-
 #pilt
-pilt1 = "botswana.jpg"
-pilt = ImageTk.PhotoImage(Image.open(pilt1))
-paneel = tk.Label(aken, image = pilt)
 
-paneel.pack(side = "bottom", fill = "both", expand = "yes")
+pilt1 = PhotoImage(file="pilt07.gif")
+louend.create_image(400, 0, anchor=NW, image=pilt1)
+
 
 
 
