5e0b3f197152d2932073d011fb5e05cab73aba3a
JassOunapuu <98941717+JassOunapuu@users.noreply.github.com>
2022-04-04 13:44:55 +0300

    Add files via upload

--- GameTest.py
+++ GameTest.py
@@ -6,15 +6,19 @@ coins = 0
 damage = 0
 
 def printParameters():
+    print("")
     print("Sul on {0} elu, {1} luumurd(u) ja {2} money.".format(hp, damage, coins))
 
 def printHp():
+    print("")
     print("Sul on", hp, "elu.")
 
 def printCoins():
+    print("")
     print("Sul on", coins, "money.")
 
 def printDamage():
+    print("")
     print("Sul on", damage, "luumurd(u).")
 
 def meetShop():
@@ -28,6 +32,7 @@ def meetShop():
             coins -= cost
             printCoins()
             return True
+        print("")
         print("Sul pole raha, mine muretse juurde")
         return False
 
@@ -45,12 +50,13 @@ def meetShop():
     print("Teel kohtasite kaupmeest!")
     printParameters()
     
-    while input("Mida sa tegema hakkad (sisse/välja): ").lower() == "sisse":
+    while input("Kas tahad midagi osta või lahkuda? (O/L): ").upper() == "O":
+        print("")
         print("1) Üks HP -", oneHpCost, "money;")
         print("2) Kolm HP -", threeHpCost, "money;")
         print("3) {0} {1} - {2} money".format(weaponRarity, weapon, weaponCost))
 
-        choice = input("Mida sa tahad osta: ")
+        choice = input("Mida tahad osta: ")
         if choice == "1":
             if buy(oneHpCost):
                 hp += 1
@@ -64,47 +70,55 @@ def meetShop():
                 damage = weaponDmg
                 printDamage()
         else:
+            print("")
             print("Ma ei müü seda.")
 
 def meetMonster():
     global hp
     global coins
 
-    monsterLvl = r.randint(1, 4)
+    monsterLvl = r.randint(1, 3)
     monsterHp = monsterLvl
     monsterDmg = monsterLvl * 2 - 1
     monsters = ["Kanged mehed Turjamaalt", "Härjapõlvlane", "Kratt", "Küla joodik", "Lumivalguke", "Saatana käsilane"]
 
     monster = r.choice(monsters)
-
+    print("")
     print("Sa kohtasid vastast - {0}, Tal on {1} level, {2} elu ja {3} luumurd(u).".format(monster, monsterLvl, monsterHp, monsterDmg))
     printParameters()
 
     while monsterHp > 0:
-        choice = input("Mida sa teed (ründa/jookse): ").lower()
+        print("")
+        choice = input("Mida sa teed ründa/jookse (R/J): ").lower()
 
-        if choice == "ründa":
+        if choice == "r":
             monsterHp -= damage
+            print("")
             print("Sa ründasid vastast ja tal on alles", monsterHp, "elu.")
-        elif choice == "jookse":
+        elif choice == "j":
             chance = r.randint(0, monsterLvl)
             if chance == 0:
+                print("")
                 print("Sul õnnestus lahinguväljalt põgeneda!")
                 break
             else:
+                print("")
                 print("Vastane oli liiga tugev ja jõudis sulle järele...")
         else:
             continue
 
         if monsterHp > 0:
             hp -= monsterDmg
-            print("Vastane ründas ja teil on alles", hp, "elu.")
+            print("")
+            print("Vastane ründas ja te kaotasite", monsterDmg, "elu.")
+            print("Teil on alles", hp, "elu.")
         
         if hp <= 0:
             break
     else:
-        loot = r.randint(0, 2) + monsterLvl
+        loot = r.randint(0, 2) + monsterLvl*2
         coins += loot
+        print("")
         print("Teil õnnestus Vastane võita, mille eest saite", loot, "money.")
         printCoins()
     
@@ -116,7 +130,7 @@ def initGame(initHp, initCoins, initDamage):
     hp = initHp
     coins = initCoins
     damage = initDamage
-
+    print("")
     print("Tere tulemast, siit algab sinu seiklus!")
     printParameters()
 
@@ -125,7 +139,7 @@ def gameLoop():
 
     if situation == 0:
         meetShop()
-    elif situation == 1:
+    elif situation >= 8:
         meetMonster()
     else:
         input("Kõnnid mööda tänavat...")
@@ -138,7 +152,9 @@ def print_menuu():
     print ("[4] Credits")
     print ("[5] Välju mängust")
     print (67 * "-")
-  
+    global valik
+    valik = int(input("Valik [1-5]: "))
+
 loop=True      
   
 
@@ -155,12 +171,11 @@ def salvestamine():
         
 while loop:   
     print_menuu()   
-    valik = int(input("Enter your valik [1-5]: "))
-     
-    if valik==1:     
+
+    if valik==1:
         print ("Tere tulemast uude mängu!")
         loop = False
-        initGame(randint(2, 6),randint(4, 8),randint(1, 3))
+        initGame(randint(1, 1),randint(4, 8),randint(1, 1))
     elif valik==2:
         print ("Salvestasid mängu")
         
@@ -168,10 +183,12 @@ while loop:
         print ("Laadisid mängu")
         
     elif valik==4:
+        print("")
         print ("Martin Pettai, Jass Õunapuu, Artjom Vinogradov")
-        
+        print("")
     elif valik==5:
         loop = False
+        exit()
     else:
         print("Vale valik, oled kindel, et vajutasid õiget klahvi?")
 
@@ -181,7 +198,7 @@ while True:
     gameLoop()
 
     if hp <= 0:
-        if input("Kas tahad uuesti mängida (jah/ei): ").lower() == "jah":
+        if input("Kas tahad menüüsse tagasi minna? (jah/ei): ").lower() == "jah":
             print_menuu()
         else:
             break
